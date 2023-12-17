bd65bede37b17cf86c45565f97f791699aa6a34a
Yieison Lizarazo <76229560+Yieison@users.noreply.github.com>
2023-12-17 22:38:42 +0000

    BD en AWS

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,23 @@
 <?php
-    $conex = mysqli_connect("localhost","root","","usuarios");
+//    $conex = mysqli_connect("localhost","root","","usuarios");
 
     // if($conex){
     //     echo "Conexion exitosa";
     // }else{
     //     echo "ERROR";
     // }
+$servername = "database-1.cr83cdsrhghv.us-east-2.rds.amazonaws.com"; // Endpoint de RDS
+$username = "admin"; // Usuario de RDS
+$password = "oU8RJC4ZaRlxjFFJzXT3"; // Contraseña de RDS
+$dbname = "database-1"; // Nombre de la base de datos
+$port = 3306;
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
 ?>
